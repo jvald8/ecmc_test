056670c4b753c5a4dba6f1798ba0756b4c883cc4
Jon Ringvald <jvald8@gmail.com>
2015-07-18 11:42:20 -0700

    add todo.md

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// where the action happens.
+var secret_stuff = require('../secret_stuff.js');
+
+alert('dog')
+
+alert(secret_stuff.SUPPORT_EMAIL)
---
+++ todo.md
@@ -0,0 +1,2 @@
+1. add browserify for src js build.
+2. register flickr app

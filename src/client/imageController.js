b67295d253db4edabd47a8fb8b0f527d68ce6bf1
Jon Ringvald <jvald8@gmail.com>
2015-07-18 16:35:21 -0700

    photo calls are working

--- src/client/imageController.js
+++ src/client/imageController.js
@@ -1,7 +1,41 @@
-// where the action happens.
-
 var flickarama = angular.module('flickarama', []);
 
 flickarama.controller('ImagesController', function($scope, $http) {
+  $scope.tag = 'dctech';
+  $scope.client_id = 'e8f708f2b0344f42b2e28293c234e69a'
   $scope.something = 'something';
+  $scope.currentTime = Math.floor(Date.now()/ 1000);
+  $scope.secondsInMonth = 2592000;
+  $scope.secondsAMonthAgo = $scope.currentTime - $scope.secondsInMonth;
+
+  // Instagram Call
+  var instagramUrl = 'https://api.instagram.com/v1/tags/' + $scope.tag + '/media/recent?client_id=' + $scope.client_id + '&min_timestamp=' + $scope.secondsAMonthAgo + '&max_timestamp=' + $scope.currentTime + '&count=' + 1000 + '&callback=JSON_CALLBACK';
+  $http.jsonp(instagramUrl)
+  .success(function(data, status, headers, config) {
+    var instagramPhotoArray = data.data;
+    console.log(instagramPhotoArray[0])
+  })
+  .error(function(data, status, headers, config) {
+    console.log('theres been an error in instagram angular')
+  })
+
+
+  // Flickr Call
+  $scope.api_key = 'd99bf6642cce654ec8deb8f81b5519ee'
+  var flickrUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + $scope.api_key + '&text=' + $scope.tag + '&min_upload_date=' + $scope.secondsAMonthAgo + '&format=json&jsoncallback=?';
+  $scope.flickrUrl = flickrUrl;
+  $http.get(flickrUrl)
+  .success(function(data, status, headers, config) {
+    var flickrObject = JSON.parse(data.substring(14, data.length - 1))
+    var flickrPhotoArray = flickrObject.photos.photo;
+    console.log(flickrPhotoArray)
+
+  })
+  .error(function(data, status, headers, config) {
+    console.log('theres been an error in flickr angular')
+  })
+
+
 })
+
+
--- todo.md
+++ todo.md
@@ -7,3 +7,4 @@ Assumptions:
 Defining the last month as the last 30 days.
 Options to get last month worth of IG photos.
 1. Assume there won't be more than 1000 tags in 30 days.
+2. chose to search for text = #dctech, because noone uses tags in flickr

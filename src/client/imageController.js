b76dd3f5f81c5e3af90edd5040b24e1a0600034a
Jon Ringvald <jvald8@gmail.com>
2015-07-18 17:16:59 -0700

    clean up html & need to add flickr and IG objects together

--- src/client/imageController.js
+++ src/client/imageController.js
@@ -3,7 +3,6 @@ var flickarama = angular.module('flickarama', []);
 flickarama.controller('ImagesController', function($scope, $http) {
   $scope.tag = 'dctech';
   $scope.client_id = 'e8f708f2b0344f42b2e28293c234e69a'
-  $scope.something = 'something';
   $scope.currentTime = Math.floor(Date.now()/ 1000);
   $scope.secondsInMonth = 2592000;
   $scope.secondsAMonthAgo = $scope.currentTime - $scope.secondsInMonth;
@@ -29,6 +28,26 @@ flickarama.controller('ImagesController', function($scope, $http) {
     var flickrObject = JSON.parse(data.substring(14, data.length - 1))
     var flickrPhotoArray = flickrObject.photos.photo;
     console.log(flickrPhotoArray)
+
+    var commentCountArray = [];
+
+    flickrPhotoArray.forEach(function(x) {
+      var commentCall = 'https://api.flickr.com/services/rest/?method=flickr.photos.comments.getList&api_key=' + $scope.api_key + '&photo_id=' + x.id + '&format=json'
+      $http.get(commentCall)
+      .success(function(data, status, headers, config) {
+        var commentObject = JSON.parse(data.substring(14, data.length - 1));
+
+        var commentCount = commentObject.comments.comment.length;
+
+        // recreate instagram object and append to the instagram array
+      })
+      .error(function(data, status, headers, config) {
+        console.log('there was an error')
+      })
+
+    })
+
+
 
   })
   .error(function(data, status, headers, config) {
